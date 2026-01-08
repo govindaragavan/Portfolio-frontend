086c858124b9d5684c4f6331036360e4aa77c975
govindaragavan <govindaragavan10@gmail.com>
2026-01-09 02:42:18 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Add custom JavaScript here
+function runTypingEffect(){
+    const text ="I'm Govindaragavan"
+    const typingElement = document.querySelector('#typing-text')
+    const typingDelay=100;
+
+    typeText(text,typingElement,typingDelay)
+}
+function typeText(text,typingElement,typingDelay){
+for(let i=0;i<text.length;i++){
+    setTimeout(()=>{
+        typingElement.textContent+=text.charAt(i);
+},typingDelay*i)
+}
+}
+document.addEventListener('DOMContentLoaded',runTypingEffect)
